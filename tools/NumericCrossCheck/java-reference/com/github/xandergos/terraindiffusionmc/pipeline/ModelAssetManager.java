79239251c94be4d6365d019f997066317d43e1ec
Bradly Tillmann <bradlytillmann@gmail.com>
2026-08-10 15:57:40 -0600

    Clean up repo

--- tools/NumericCrossCheck/java-reference/com/github/xandergos/terraindiffusionmc/pipeline/ModelAssetManager.java
+++
@@ -1,8 +0,0 @@
-package com.github.xandergos.terraindiffusionmc.pipeline;
-import java.nio.file.Path;
-public final class ModelAssetManager {
-    public static void ensureAssetsReady() {}
-    public static Path resolveAssetPath(String f) {
-        return Path.of(System.getProperty("user.home"), ".config/VintagestoryData/TerrainDiffusionModels", f);
-    }
-}
--- tools/NumericCrossCheck/java-reference/com/github/xandergos/terraindiffusionmc/pipeline/WorldPipelineModelConfig.java
+++
@@ -1,6 +0,0 @@
-package com.github.xandergos.terraindiffusionmc.pipeline;
-public final class WorldPipelineModelConfig {
-    public static float[] frequencyMultipliers() { return new float[]{1f,1f,1f,1f,1f}; }
-    public static float nativeResolution() { return 30f; }
-    public static int latentCompression() { return 8; }
-}
